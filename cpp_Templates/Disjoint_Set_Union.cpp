c12c7a831e91486750e8986af2c90141e8304870
Iris6373 <75437488+Iris6373@users.noreply.github.com>
2021-11-17 12:49:09 +0530

    Create Disjoint_Set_Union.cpp

---
+++ cpp_Templates/Disjoint_Set_Union.cpp
@@ -0,0 +1,46 @@
+// DSU or Union-Find using Union by size and path compression
+
+class DSU {
+    public:
+    vector<int> parent, size;
+    
+    DSU (int n) {
+        for (int i=0;i<=n;i++) {
+            parent.push_back(i);
+            size.push_back(1);
+        }
+    }
+    
+    // path compression
+    int findPar(int node) {
+        if (parent[node] == node) return node;
+        return parent[node] = findPar(parent[node]);
+    }
+    
+    // Union by size
+    void unionS(int u, int v) {
+        int pu = findPar(u);
+        int pv = findPar(v);
+        
+        if (pu == pv) return;
+        
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+    
+    void print() {
+        for (int i=0;i<parent.size();i++) {
+            cout << i << " " << parent[i] << " " << size[i] << "\n";
+        }
+    }
+};
+
+// DSU dsu(n);
+// dsu.findPar(i);
+// dsu.unionS(i,j);
